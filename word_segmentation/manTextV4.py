90d335cf244e32a2949634a2b56ce4b4fe7f1329
pant411 <pantchanit2561@hotmail.com>
2021-11-17 22:30:10 +0700

    ...

--- word_segmentation/manTextV4.py
+++ word_segmentation/manTextV4.py
@@ -1,49 +1,69 @@
 #! -*- coding: UTF8 -*-
 from wordcut import Wordcut
 import pylcs
+import pyuca
+
 
 def read_text(file):
     #doc = input("file: ")
-    data = open("docs_for_test/{}.txt".format(file),"r")
+    data = open("docs_for_test/{}.txt".format(file), "r")
     return data
 
-def read_dict():
-    with open('dict.txt', encoding="UTF-8") as dict_file:
-        word_list = list(set([w.rstrip() for w in dict_file.readlines()]))
-        wordcut = Wordcut(word_list) 
+
+def read_dict(choose="dict.txt"):
+    with open(choose, encoding="UTF-8") as dict_file:
+        dict_file2 = sorted(dict_file, key=pyuca.Collator().sort_key)
+        word_list = list(set([w.rstrip() for w in dict_file2]))
+        wordcut = Wordcut(word_list)
+    #print([w.rstrip() for w in dict_file2])
     return wordcut
 
-def select_tag(indexOFchosen):
-    if indexOFchosen <= 1: op = 1 #store in org
-    elif indexOFchosen >= 7: op = 7 #store in byuser   
-    else: op = indexOFchosen #
-    return op 
 
-def store_tag(op,text,org,tel,topic,toUser,byUser,date,no):
-    if op == 1: org.append(text)
-    elif op == 2: topic.append(text)
-    elif op == 3: toUser.append(text)
-    elif op == 4: date.append(text)
-    elif op == 5: tel.append(text)
-    elif op == 6: no.append(text)
-    elif op >= 7: byUser.append(text)
-    return org,tel,topic,toUser,byUser,date,no
+def select_tag(indexOFchosen):
+    if indexOFchosen <= 1:
+        op = 1  # store in org
+    elif indexOFchosen >= 7:
+        op = 7  # store in byuser
+    else:
+        op = indexOFchosen
+    return op
+
+
+def store_tag(op, text, org, tel, topic, toUser, byUser, date, no):
+    if op == 1:
+        org.append(text)
+    elif op == 2:
+        topic.append(text)
+    elif op == 3:
+        toUser.append(text)
+    elif op == 4:
+        date.append(text)
+    elif op == 5:
+        tel.append(text)
+    elif op == 6:
+        no.append(text)
+    elif op >= 7:
+        byUser.append(text)
+    return org, tel, topic, toUser, byUser, date, no
+
 
 def read_keyword():
     my_file = open("keyword.txt", "r")
     content = my_file.read()
     content_list = content.split(",")
-    my_file.close()    
+    my_file.close()
     return content_list
+
 
 def read_key(file):
     my_file = open(f"docs_source/{file}_key.txt", "r")
     content = my_file.read()
     content_list = content.split(",")
-    my_file.close()    
+    my_file.close()
     return content_list
 
-def org_tag(ele,tag1):
+
+def org_tag(ele, tag1):
     x_tag1 = ele.find('ภาควิชา')
     x_tag2 = ele.find('คณะ')
     x_tag3 = ele.find('มหาวิทยาลัย')
@@ -54,15 +74,24 @@ def org_tag(ele,tag1):
     x_tag8 = ele.find('สถาน')
     x_tag9 = ele.find('บริษัท')
     res = ''
-    if x_tag1 != -1: res = ele[x_tag1:len(ele)-1]
-    elif x_tag2 != -1: res = ele[x_tag2:len(ele)-1]
-    elif x_tag3 != -1: res = ele[x_tag3:len(ele)-1]
-    elif x_tag4 != -1: res = ele[x_tag4:len(ele)-1]
-    elif x_tag5 != -1: res = ele[x_tag5:len(ele)-1]
-    elif x_tag6 != -1: res = ele[x_tag6:len(ele)-1]
-    elif x_tag7 != -1: res = ele[x_tag7:len(ele)-1]
-    elif x_tag8 != -1: res = ele[x_tag8:len(ele)-1]
-    elif x_tag9 != -1: res = ele[x_tag9:len(ele)-1]
+    if x_tag1 != -1:
+        res = ele[x_tag1:len(ele)-1]
+    elif x_tag2 != -1:
+        res = ele[x_tag2:len(ele)-1]
+    elif x_tag3 != -1:
+        res = ele[x_tag3:len(ele)-1]
+    elif x_tag4 != -1:
+        res = ele[x_tag4:len(ele)-1]
+    elif x_tag5 != -1:
+        res = ele[x_tag5:len(ele)-1]
+    elif x_tag6 != -1:
+        res = ele[x_tag6:len(ele)-1]
+    elif x_tag7 != -1:
+        res = ele[x_tag7:len(ele)-1]
+    elif x_tag8 != -1:
+        res = ele[x_tag8:len(ele)-1]
+    elif x_tag9 != -1:
+        res = ele[x_tag9:len(ele)-1]
     '''space = res.find(' ')
     if space != -1:
         res = res[:space]'''
@@ -70,47 +99,51 @@ def org_tag(ele,tag1):
         tag1.append(res)
     return tag1
 
+
 def main_mantext(file):
     data = read_text(file)
     keyword = read_keyword()
     line_no = 0
-    org,tel,topic,toUser,byUser,date,no = [],[],[],[],[],[],[] #org=ส่วนงานหรือส่วนราชการ tel=เบอร์โทร topic=เรื่อง toUser=เรียน byUser=คนเซ็น date=วันที่ no=ที่ศธ
-    tag1 = [] #tag1=tagสถานที่
+    # org=ส่วนงานหรือส่วนราชการ tel=เบอร์โทร topic=เรื่อง toUser=เรียน byUser=คนเซ็น date=วันที่ no=ที่ศธ
+    org, tel, topic, toUser, byUser, date, no = [], [], [], [], [], [], []
+    tag1 = []  # tag1=tagสถานที่
     wordcut = read_dict()
     select_list_org = -1
     status_select_org = True
     for line in data.readlines():
         res = ''
-        tag1 = org_tag(line,tag1)
+        tag1 = org_tag(line, tag1)
         inline = wordcut.tokenize(line)
         inline = list(map(lambda s: s.strip(), inline))
         inline.append('\n')
-        lock_store = True #สถานะการเก็บข้อมูลลงlist
+        lock_store = True  # สถานะการเก็บข้อมูลลงlist
         op = -1
         for ele in inline:
-            candidate = pylcs.lcs_of_list(ele,keyword)
+            candidate = pylcs.lcs_of_list(ele, keyword)
             chosen = max(candidate)
             indexOFchosen = candidate.index(chosen)
             if select_list_org == -1 and status_select_org:
-                if len('บันทึกข้อความ') - pylcs.lcs("บันทึกข้อความ",ele) <=3:
-                    select_list_org = 1 #use org
+                if len('บันทึกข้อความ') - pylcs.lcs("บันทึกข้อความ", ele) <= 3:
+                    select_list_org = 1  # use org
                     status_select_org = False
             if (abs(len(keyword[indexOFchosen]) - chosen) <= 1) or '\n' in ele or ele in keyword:
-                if (lock_store == False and line_no > 0) or '\n' in ele:      
-                    org,tel,topic,toUser,byUser,date,no = store_tag(op,res,org,tel,topic,toUser,byUser,date,no)
+                if (lock_store == False and line_no > 0) or '\n' in ele:
+                    org, tel, topic, toUser, byUser, date, no = store_tag(
+                        op, res, org, tel, topic, toUser, byUser, date, no)
                     res = ''
                     lock_store = True
                 if lock_store == True and '\n' not in ele:
                     op = select_tag(indexOFchosen)
                     lock_store = False
                 line_no += 1
-                continue             
+                continue
             if lock_store == False and ele != ')':
-                if ('ดร' in ele or 'ดร.' in ele or 'ตร.' in ele or 'ตร' in ele or ele == ' ') and op == 7: continue
+                if ('ดร' in ele or 'ดร.' in ele or 'ตร.' in ele or 'ตร' in ele or ele == ' ') and op == 7:
+                    continue
                 res = res + ele
                 if (op == 7 or op == 4):
                     res += ' '
-        line_no += 1  
+        line_no += 1
     #print(f'org: {org}')
     #print(f'topic: {topic}')
     #print(f'toUser: {toUser}')
@@ -118,14 +151,16 @@ def main_mantext(file):
     #print(f'tel: {tel}')
     #print(f'date: {date}')
     #print(f'no: {no}')
-    #print(f'tag1: {tag1}')  
-    #print(org[0],topic[0],toUser[0],tel[0],date[0])   
+    #print(f'tag1: {tag1}')
+    # print(org[0],topic[0],toUser[0],tel[0],date[0])
     #print(f'select {select_list_org}')
     select_org = []
     if select_list_org == 1:
-        if len(org) > 0: select_org.append(org[0])
+        if len(org) > 0:
+            select_org.append(org[0])
     elif select_list_org == -1:
-        if len(tag1) > 0: select_org.append(tag1[0])
+        if len(tag1) > 0:
+            select_org.append(tag1[0])
     index_org = 0
     if len(select_org) == 0:
         select_org.append("ไม่พบข้อมูล")
@@ -148,13 +183,15 @@ def main_mantext(file):
     #print(f'วันที่ี: {date[0]}')
     #print(f'คนเช็น: {byUser[-1]}')
     #print(f'ที่: {no[0]}')
-    return [select_org[index_org],topic[0],toUser[0],tel[0],date[0],byUser[-1]]
+    return [select_org[index_org], topic[0], toUser[0], tel[0], date[0], byUser[-1]]
 
-def write_txt(inPut,file):
+
+def write_txt(inPut, file):
     with open('file_txt/a_'+file+'.txt', 'w') as f:
         for line in inPut:
             f.write(line)
-            f.write('\n')   
+            f.write('\n')
+
 
 def test_dict(file):
     res = main_mantext(file)
@@ -168,18 +205,21 @@ def test_dict(file):
     print(wordcut.tokenize(res[4]))
     print(wordcut.tokenize(res[5]))
 
+
 def add_space(text):
     res = ''
-    lnum = ['๐','๑','๒','๓','๔','๕','๖','๗','๘','๙','1','2','3','4','5','6','7','8','9','0']
-    character = ['(',')','|','!','@','&','“']
+    lnum = ['๐', '๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    character = ['(', ')', '|', '!', '@', '&', '“']
     for i in range(len(text)):
         if text[i] in lnum and text[i-1] not in lnum:
             res += (' '+text[i])
-        elif  text[i] in character and text[i-1] not in character:
+        elif text[i] in character and text[i-1] not in character:
             res += (' '+text[i]+' ')
         else:
             res += text[i]
     return res
+
 
 def count_score(file):
     res = main_mantext(file)
@@ -187,39 +227,47 @@ def count_score(file):
         word_list = list(set([w.rstrip() for w in dict_file.readlines()]))
         wordcut = Wordcut(word_list)
     key = read_key(file)
-    score_result = [0,0,0,0,0,0]
+    score_result = [0, 0, 0, 0, 0, 0]
     score_full = []
     for i in range(6):
         res[i] = add_space(res[i])
         it = wordcut.tokenize(res[i])
         space = 0
         for wc in it:
-            if wc == ' ' or wc == '  ' or wc == '   ' or wc == '    ' or wc == '     ': 
+            if wc == ' ' or wc == '  ' or wc == '   ' or wc == '    ' or wc == '     ':
                 space += 1
                 continue
-            if key[i].find(wc) != -1 :
+            if key[i].find(wc) != -1:
                 score_result[i] += 1
         score_full.append(len(it)-space)
-    #print(res)
-    #print(score_result)
-    #print(score_full)
-    print(f'ส่วนราชการ หรือ ส่วนงาน: {wordcut.tokenize(res[0])} find->{score_result[0]} total->{score_full[0]}')
-    print(f'เรื่อง: {wordcut.tokenize(res[1])} find->{score_result[1]} total->{score_full[1]}')
-    print(f'เรียน: {wordcut.tokenize(res[2])} find->{score_result[2]} total->{score_full[2]}')
-    print(f'โทร: {wordcut.tokenize(res[3])} find->{score_result[3]} total->{score_full[3]}')
-    print(f'วันที่: {wordcut.tokenize(res[4])} find->{score_result[4]} total->{score_full[4]}')
-    print(f'คนเช็น: {wordcut.tokenize(res[5])} find->{score_result[5]} total->{score_full[5]}')
+    # print(res)
+    # print(score_result)
+    # print(score_full)
+    print(
+        f'ส่วนราชการ หรือ ส่วนงาน: {wordcut.tokenize(res[0])} find->{score_result[0]} total->{score_full[0]}')
+    print(
+        f'เรื่อง: {wordcut.tokenize(res[1])} find->{score_result[1]} total->{score_full[1]}')
+    print(
+        f'เรียน: {wordcut.tokenize(res[2])} find->{score_result[2]} total->{score_full[2]}')
+    print(
+        f'โทร: {wordcut.tokenize(res[3])} find->{score_result[3]} total->{score_full[3]}')
+    print(
+        f'วันที่: {wordcut.tokenize(res[4])} find->{score_result[4]} total->{score_full[4]}')
+    print(
+        f'คนเช็น: {wordcut.tokenize(res[5])} find->{score_result[5]} total->{score_full[5]}')
 
-def display(file,write_txt=False):
+
+def display(file, write_txt=False):
     res = main_mantext(file)
-    #print(res)
+    # print(res)
     print(f'ส่วนราชการ หรือ ส่วนงาน: {res[0]}')
     print(f'เรื่อง: {res[1]}')
     print(f'เรียน: {res[2]}')
     print(f'โทร: {res[3]}')
     print(f'วันที่: {res[4]}')
     print(f'คนเช็น: {res[5]}')
-    if write_txt: write_txt(res,file)
+    if write_txt:
+        write_txt(res, file)
 
-#display()
-#main_mantext()
+# display()
+# main_mantext()
