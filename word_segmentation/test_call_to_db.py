dd6fc771fae12be6bdb2d9867aacff0c5e19e7ef
pant411 <pantchanit2561@hotmail.com>
2021-11-21 14:04:36 +0700

    del

--- word_segmentation/test_call_to_db.py
+++ word_segmentation/test_call_to_db.py
@@ -3,4 +3,4 @@ from database_update import newdocumentAdd
 
 fileName = input("file: ") #เลือกไฟล์ test001 ถึง test015 เท่านั้น
 res0,res1,res2,res5,day,month,year = send2db(fileName)
-newdocumentAdd("http://www.copperpod.cpe.ku.ac.th/~pluem/Iris_project/word_segmentation/docs_source/"+fileName+".pdf", res1, res5, res0, res2, "สังกัดผู้รับ", "เนื้อหา", year, month, day)
+newdocumentAdd("/~pluem/Iris_project/word_segmentation/docs_source/"+fileName+".pdf", res1, res5, res0, res2, "สังกัดผู้รับ", "เนื้อหา", year, month, day)
